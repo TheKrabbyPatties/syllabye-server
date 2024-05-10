6cb1f8e107bde116e7b56359236955c93ffa3b88
Kevin <kevinadanowski@lewisu.edu>
2024-05-10 08:59:11 -0500

    Changed the url ending for the json

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.get('/api/ping', (request, response) => {
 
 app.use(express.json({ limit: '50mb' }));
 
-app.post('/endpoint', (req, res) => {
+app.post('/save/json', (req, res) => {
     const data = req.body; 
     const filePath = './JSON/file.json';
 
