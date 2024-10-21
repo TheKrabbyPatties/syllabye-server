d84ff094bbddf2be5db6b1be62688bf5b1b2092c
olivia1117 <oadamic@gmail.com>
2024-10-21 14:40:15 -0500

    committing rolled back changes

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-const express = require('express');
+const express = require('express'),
+app = express();
+
+// const express = require('express');
 const bodyParser = require('body-parser');
 const { initializeApp } = require('firebase/app');
 const { getDatabase, ref, set } = require('firebase/database');
